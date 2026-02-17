33f2cef4da236e80a3e1b85334d34823ee29d9c8
Justin Trahan <justin@trahan.dev>
2026-02-17 04:53:24 +0000

    format ssh_helpers

--- ssh_helpers.sh
+++ ssh_helpers.sh
@@ -1,17 +1,17 @@
 # Function to start an interactive shell in the specified pod
 purgessh() {
-	local hostname=$1
-	if [[ -z "$hostname" ]]; then
-		echo "Please specify ssh hostname. eg: jtrahan@10.0.0.32"
-		return 1
-	fi
-	ssh-keygen -R $hostname
+  local hostname=$1
+  if [[ -z "$hostname" ]]; then
+    echo "Please specify ssh hostname. eg: jtrahan@10.0.0.32"
+    return 1
+  fi
+  ssh-keygen -R $hostname
 }
 
 # Auto-completion for pod names
 _purgessh_completion() {
-	local pods=($(awk '{print $1}' ~/.ssh/known_hosts 2>/dev/null))
-	_describe 'hosts' hosts
+    local pods=($(awk '{print $1}' ~/.ssh/known_hosts 2>/dev/null))
+    _describe 'hosts' hosts
 }
 
 # Register the auto-completion for the purgessh function
@@ -21,18 +21,18 @@ alias purge-ssh-host=purgessh
 
 # Function to start an interactive shell in the specified pod
 getknownhost() {
-	local hostname=$1
-	if [[ -z "$hostname" ]]; then
-		echo "Please specify ssh hostname. eg: jtrahan@10.0.0.32"
-		return 1
-	fi
-	sed -n "/$hostname/p" ~/.ssh/known_hosts
+  local hostname=$1
+  if [[ -z "$hostname" ]]; then
+    echo "Please specify ssh hostname. eg: jtrahan@10.0.0.32"
+    return 1
+  fi
+  sed -n "/$hostname/p" ~/.ssh/known_hosts
 }
 
 # Auto-completion for pod names
 _getknownhost_completion() {
-	local pods=($(awk '{print $1}' ~/.ssh/known_hosts 2>/dev/null))
-	_describe 'hosts' hosts
+  local pods=($(awk '{print $1}' ~/.ssh/known_hosts 2>/dev/null))
+  _describe 'hosts' hosts
 }
 
 # Register the auto-completion for the purgessh function
